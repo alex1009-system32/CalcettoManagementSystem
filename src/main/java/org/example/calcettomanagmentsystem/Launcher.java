9ecf5ac8e31f8e2f6e66808f736863b3c035c4ed
alex <kerschbamer.2009.alex@gmail.com>
2026-02-03 18:01:03 +0100

    Added JavaFX Structure

---
+++ src/main/java/org/example/calcettomanagmentsystem/Launcher.java
@@ -0,0 +1,9 @@
+package org.example.calcettomanagmentsystem;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
