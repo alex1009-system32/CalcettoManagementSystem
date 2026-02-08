43430d7c07ac43e9d4a51cbe487917660cf84d05
alex <kerschbamer.2009.alex@gmail.com>
2026-02-08 21:16:42 +0100

    changes in config

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,6 +12,7 @@ module org.example.calcettomanagmentsystem {
     requires com.almasb.fxgl.all;
     requires java.sql;
     requires annotations;
+    requires org.postgresql.jdbc;
 
     opens org.example.calcettomanagmentsystem to javafx.fxml;
     exports org.example.calcettomanagmentsystem;
